53f6a2b271163adf7d2bac5ae103342bfefc4082
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-15 11:11:00 -0300

    feat: Add blog post types

---
+++ src/post/blog-post.ts
@@ -0,0 +1,29 @@
+export type PostMetadata = Readonly<{
+  title: string;
+  id: string;
+  timestamp: number;
+  image: {
+    src: string;
+    alt: string;
+  };
+  description: string;
+  tags: string[];
+}>;
+
+export type BlogPost = Readonly<{
+  metadata: PostMetadata;
+  body: string;
+}>;
+
+export type PostQuery = Readonly<{
+  year: number;
+  id: string;
+}>;
+
+export function yearOfPost(post: PostMetadata) {
+  return new Date(post.timestamp).getFullYear();
+}
+
+export function isQueriedPost(query: PostQuery, post: PostMetadata): boolean {
+  return yearOfPost(post) === query.year && post.id === query.id;
+}
