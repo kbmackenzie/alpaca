65265a88b8fe4f44db08ec23f44f608f6cf3829f
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-16 03:28:09 -0300

    feat(post): Make post IDs contain only lowercase characters

--- src/post/post-id.ts
+++ src/post/post-id.ts
@@ -1,10 +1,10 @@
 const idChar: RegExp = /^[a-zA-Z0-9\-_]$/;
-const idRe:   RegExp = /^[a-zA-Z0-9\-_]+$/;
+const idRe:   RegExp = /^[a-z0-9\-_]+$/;
 
 export function toPostID(id: string): string {
   const output: string[] = [];
   for (const char of id) {
-    output.push(idChar.test(char) ? char : '-');
+    output.push(idChar.test(char) ? char.toLowerCase() : '-');
   }
   return output.join('');
 }
