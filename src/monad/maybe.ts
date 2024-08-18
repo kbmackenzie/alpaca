bf45c4556a131e171a5418658cb5e0746c2b32a9
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-18 13:31:21 -0300

    feat(monad): Add 'pure', 'bind', and more to Maybe<T>

--- src/monad/maybe.ts
+++ src/monad/maybe.ts
@@ -1,25 +1,45 @@
-export type Maybe<T> =
-  | { type: 'just', value: T }
+export type Maybe<A> =
+  | { type: 'just', value: A }
   | { type: 'nothing' };
 
-export function isJust<T>(maybe: Maybe<T>): boolean {
+export function pure<A>(value: A): Maybe<A> {
+  return {
+    type: 'just',
+    value: value,
+  };
+}
+
+export function bind<A, B>(m: Maybe<A>, f: (a: A) => Maybe<B>): Maybe<B> {
+  if (m.type === 'nothing') return m;
+  return f(m.value);
+}
+
+export function then<A, B>(ma: Maybe<A>, mb: Maybe<B>): Maybe<B> {
+  return bind(ma, _ => mb);
+}
+
+export function fmap<A, B>(f: (a: A) => B, ma: Maybe<A>): Maybe<B> {
+  return bind(ma, (a) => pure(f(a)));
+}
+
+export function isJust<A>(maybe: Maybe<A>): boolean {
   return maybe.type === 'just';
 }
 
-export function isNothing<T>(maybe: Maybe<T>): boolean {
+export function isNothing<A>(maybe: Maybe<A>): boolean {
   return maybe.type === 'nothing';
 }
 
 /* Equivalent to Haskell's 'fromMaybe' function from Data.Maybe:
  * https://hackage.haskell.org/package/base-4.20.0.1/docs/Data-Maybe.html#v:fromMaybe */
-export function fromMaybe<T>(maybe: Maybe<T>, def: T): T {
+export function fromMaybe<A>(maybe: Maybe<A>, def: A): A {
   if (maybe.type === 'nothing') return def;
   return maybe.value;
 }
 
 /* Equivalent to Haskell's 'maybe' function from Data.Maybe:
  * https://hackage.haskell.org/package/base-4.20.0.1/docs/Data-Maybe.html#v:maybe */
-export function transformMaybe<T1, T2>(maybe: Maybe<T1>, def: T2, transform: (t: T1) => T2) {
+export function transformMaybe<A1, A2>(maybe: Maybe<A1>, def: A2, transform: (t: A1) => A2) {
   if (maybe.type === 'nothing') return def;
   return transform(maybe.value);
 }
