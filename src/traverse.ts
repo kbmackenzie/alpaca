39d0a0507053b8d29c8db4daacebad32fdc4644d
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-17 21:51:37 -0300

    feat: Add post search logic

---
+++ src/traverse.ts
@@ -0,0 +1,48 @@
+import { opendir } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import path from 'node:path';
+
+export const postFile = 'post.md';
+export const metaFile = 'meta.yaml';
+
+type Traversal = Readonly<{
+  root: string;
+  posts: string[];
+}>;
+
+export async function findPosts(root: string): Promise<string[]> {
+  const traversal = await traverseRoot(root);
+  return traversal.posts;
+}
+
+/* The voyage to the corner of the globe is a real trip. */
+async function traverseRoot(root: string): Promise<Traversal> {
+  const traversal: Traversal = {
+    root:  path.normalize(root),
+    posts: [],
+  };
+  await searchPostsDFS(traversal, root);
+  return traversal;
+}
+
+/* Depth-first search. */
+async function searchPostsDFS(traversal: Traversal, folder: string): Promise<void> {
+  const dir = await opendir(folder);
+
+  for await (const entry of dir) {
+    if (!entry.isDirectory()) continue;
+    const entryPath = path.resolve(folder, entry.name);
+
+    if (isPostFolder(entryPath)) {
+      const relative = path.relative(traversal.root, entryPath);
+      traversal.posts.push(relative);
+      continue;
+    }
+    await searchPostsDFS(traversal, entryPath)
+  }
+}
+
+function isPostFolder(folder: string) {
+  const post = path.resolve(folder, postFile);
+  return existsSync(post);
+}
