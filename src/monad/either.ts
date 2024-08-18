390d247bd43691cb36c5cb4462197513e5195161
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-18 13:36:11 -0300

    feat(monad): Add 'left' and 'right' utils, make Either<T> readonly

--- src/monad/either.ts
+++ src/monad/either.ts
@@ -1,8 +1,8 @@
 /* Note: Module should be imported qualified; i.e. import * as Either from '@/utils/either' */;
 
 export type Either<E, A> =
-  | { type: 'left' , value: E }
-  | { type: 'right', value: A };
+  | Readonly<{ type: 'left' , value: E }>
+  | Readonly<{ type: 'right', value: A }>;
 
 export function pure<E, A>(value: A): Either<E, A> {
   return {
@@ -22,4 +22,21 @@ export function then<E, A, B>(ma: Either<E, A>, mb: Either<E, B>): Either<E, B> 
 
 export function fmap<E, A, B>(f: (value: A) => B, m: Either<E, A>): Either<E, B> {
   return bind(m, (a) => pure(f(a)));
+}
+
+export function left<E, A>(e: E): Either<E, A> {
+  return {
+    type: 'left',
+    value: e,
+  };
+}
+
+export const right = pure;
+
+export function isLeft<E, A>(m: Either<E, A>): boolean {
+  return m.type === 'left';
+}
+
+export function isRight<E, A>(m: Either<E, A>): boolean {
+  return m.type === 'right';
 }
