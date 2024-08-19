93ead6854f5976575693252f76e909dc1be46a34
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-19 16:10:08 -0300

    feat(markdown): Write remark plugin for resolving image aliases

---
+++ src/markdown/image-alias.ts
@@ -0,0 +1,31 @@
+import { parseImagePath, shouldTransform } from '@/parse/image-path';
+import { isLeft } from '@/monad/either';
+import { Root } from 'mdast';
+import { Preset, Settings } from 'unified';
+import { visit } from 'unist-util-visit';
+import { join } from 'node:path/posix';
+
+type Options = {
+  resolve: boolean;
+  imageRoot: string;
+};
+
+export function resolveImageAlias(options?: Options): (tree: Root) => void {
+  if (!options?.resolve) return (_) => {};
+  return (tree) => {
+    visit(tree, 'image', node => {
+      if (!shouldTransform(node.url)) return;
+
+      const relative = parseImagePath(node.url);
+      if (isLeft(relative)) {
+        return; /* todo: handle gracefully. */
+      }
+      node.url = join(options.imageRoot, relative.value);
+    });
+  }
+}
+
+export const imageAliasPreset = (settings: Settings): Preset => ({
+  plugins: [ resolveImageAlias ],
+  settings: settings,
+});
