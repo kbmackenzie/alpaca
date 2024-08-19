f14688fa5bcf86690d4d0a7975f16c23e687d67a
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-18 23:40:15 -0300

    feat(parse): Make image parser always generate a relative path

--- src/parse/image-path.ts
+++ src/parse/image-path.ts
@@ -1,14 +1,14 @@
 import { Either, left, right } from '@/monad/either';
-import { resolve } from 'node:path';
 
 const imageAlias = /^\@kestrel\/(.*)$/;
 
-export function transformImagePath(input: string, imageRoot: string): Either<string, string> {
+/* Always returns a *relative path* if successful! */
+export function transformImagePath(input: string): Either<string, string> {
   const pathMatch = imageAlias.exec(input);
   if (pathMatch === null) {
     return left(`Couldn't parse image path: "${input}"`);
   }
-  return right(resolve(imageRoot, pathMatch[1]));
+  return right(pathMatch[1]);
 }
 
 export function shouldTransform(input: string): boolean {
