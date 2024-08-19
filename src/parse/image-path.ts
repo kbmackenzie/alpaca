c2872d2e2bfeda262b52fa483f1947b089907bc2
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-19 16:09:44 -0300

    refactor(parse): Rename image alias parser function

--- src/parse/image-path.ts
+++ src/parse/image-path.ts
@@ -3,7 +3,7 @@ import { Either, left, right } from '@/monad/either';
 const imageAlias = /^\@kestrel\/(.*)$/;
 
 /* Always returns a *relative path* if successful! */
-export function transformImagePath(input: string): Either<string, string> {
+export function parseImagePath(input: string): Either<string, string> {
   const pathMatch = imageAlias.exec(input);
   if (pathMatch === null) {
     return left(`Couldn't parse image path: "${input}"`);
