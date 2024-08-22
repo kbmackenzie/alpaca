2b2041c1422d6a3def8a1d9fc16edcdc1dc9a74e
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-22 03:24:11 -0300

    feat(config): Add config validation schema + logic

---
+++ src/config/read-config.ts
@@ -0,0 +1,33 @@
+import { KestrelConfig, defaultConfig } from '@/config/kestrel-config';
+import { Either } from '@/monad/either';
+import * as either from '@/monad/either';
+import type { JTDSchemaType } from 'ajv/dist/core';
+import Ajv from 'ajv';
+
+const schema: JTDSchemaType<KestrelConfig> = {
+  optionalProperties: {
+    appendDate: { type: 'boolean' },
+    neverInferDate: { type: 'boolean' },
+    optimizeImages: { type: 'boolean' },
+    images: {
+      properties: {
+        imageRoot: { type: 'string' },
+      },
+      optionalProperties: {
+        resolveAlias: { type: 'boolean' },
+      },
+    },
+    imageExtensions: { elements: { type: 'string' } },
+  },
+  additionalProperties: false,
+};
+
+const ajv = new Ajv();
+const validate = ajv.compile<KestrelConfig>(schema);
+
+export function validateConfig(config: object, useDefault?: boolean): Either<string, KestrelConfig> {
+  if (useDefault) return either.right(defaultConfig);
+  return validate(config)
+    ? either.right(config)
+    : either.left(`Invalid config data! Errors: ${validate.errors}`);
+}
