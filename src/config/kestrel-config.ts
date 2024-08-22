52bfdc2e93a45335939bd280c625b4326329a5f3
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-22 02:00:39 -0300

    feat(config): Add 'optimizeImages' field

--- src/config/kestrel-config.ts
+++ src/config/kestrel-config.ts
@@ -1,6 +1,7 @@
 export type KestrelConfig = {
   appendDate?: boolean;
   neverInferDate?: boolean;
+  optimizeImages?: boolean;
   images?: {
     resolveAlias: boolean;
     imageRoot: string;
@@ -12,5 +13,6 @@ export const defaultImageExtensions = ['png', 'jpg', 'webp', 'gif', 'bmp'];
 export const defaultConfig: KestrelConfig = {
   appendDate: false,
   neverInferDate: false,
+  optimizeImages: false,
   imageExtensions: defaultImageExtensions,
 };
