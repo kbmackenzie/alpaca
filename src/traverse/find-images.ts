81b7a7b8bfde679ba5c38e8fc7e3ab39751c5a58
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-22 02:49:13 -0300

    feat(traverse): Add image search logic

---
+++ src/traverse/find-images.ts
@@ -0,0 +1,27 @@
+import { Either } from '@/monad/either';
+import * as either from '@/monad/either';
+import { KestrelConfig, defaultImageExtensions } from '@/config/kestrel-config';
+import { tryGlob } from '@/safe/glob';
+import {tryCopyFile} from '@/safe/io';
+
+export async function globImages(config: KestrelConfig, folder: string): Promise<Either<string, string[]>> {
+  return tryGlob(folder, {
+    expandDirectories: {
+      extensions: config.imageExtensions ?? defaultImageExtensions,
+    },
+  });
+}
+
+export async function copyImages(config: KestrelConfig, from: string, to: string): Promise<Either<string, string[]>> {
+  const imagePaths = await globImages(config, from);
+  if (either.isLeft(imagePaths)) return imagePaths;
+
+  /* Store error messages; continue even after one copy fails. */
+  const errorList: string[] = [];
+
+  for (const image of imagePaths.value) {
+    const result = await tryCopyFile(image, to);
+    if (either.isLeft(result)) { errorList.push(result.value); }
+  }
+  return either.right<string, string[]>(errorList);
+}
