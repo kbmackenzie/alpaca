f78045cd5316cc895e8bf9c0b72e758f443eba4f
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-23 02:41:20 -0300

    refactor(traverse): A little clean-up in image search logic

--- src/traverse/find-images.ts
+++ src/traverse/find-images.ts
@@ -2,9 +2,12 @@ import { Either } from '@/monad/either';
 import * as either from '@/monad/either';
 import { KestrelConfig, defaultImageExtensions } from '@/config/kestrel-config';
 import { tryGlob } from '@/safe/glob';
-import {tryCopyFile} from '@/safe/io';
+import { tryCopyFile } from '@/safe/io';
 
-export async function globImages(config: KestrelConfig, folder: string): Promise<Either<string, string[]>> {
+export async function globImages(
+  config: KestrelConfig,
+  folder: string
+): Promise<Either<string, string[]>> {
   return tryGlob(folder, {
     expandDirectories: {
       extensions: config.imageExtensions ?? defaultImageExtensions,
@@ -12,7 +15,11 @@ export async function globImages(config: KestrelConfig, folder: string): Promise
   });
 }
 
-export async function copyImages(config: KestrelConfig, from: string, to: string): Promise<Either<string, string[]>> {
+export async function copyImages(
+  config: KestrelConfig,
+  from: string,
+  to: string
+): Promise<Either<string, string[]>> {
   const imagePaths = await globImages(config, from);
   if (either.isLeft(imagePaths)) return imagePaths;
 
