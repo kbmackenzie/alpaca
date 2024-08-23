8a977cef6765cae12622cca72df7813f01c277d6
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-23 01:28:25 -0300

    feat(compile): Accept KestrelConfig in post compiler functions

--- src/compile/post.ts
+++ src/compile/post.ts
@@ -1,5 +1,6 @@
 import { Either } from '@/monad/either';
 import * as either from '@/monad/either';
+import { KestrelConfig } from '@/config/kestrel-config';
 import { getPostDate } from '@/parse/date';
 import { toPostID } from '@/post/id';
 import { Post, PostMetadata } from '@/post/post';
@@ -9,24 +10,24 @@ import { resolveImageAlias } from '@/markdown/image-alias';
 import { remark } from 'remark';
 
 export async function compilePost(
+  config: KestrelConfig,
   info: PostInfo,
-  imageRoot: string
 ): Promise<Either<string, string>> {
   const postM = await either.bindAsync(
     readPostData(info.path),
-    (postData) => createPost(info, postData, imageRoot)
+    (postData) => createPost(config, info, postData)
   );
   return either.fmap(post => JSON.stringify(post), postM);
 }
 
 export async function createPost(
+  config: KestrelConfig,
   info: PostInfo,
   postData: PostData,
-  imageRoot: string
 ): Promise<Either<string, Post>> {
   const id   = toPostID(info.relative);
   const date = await getPostDate(info.path, postData.meta.date)
-  const body = await transformContent(postData.body, imageRoot);
+  const body = await transformContent(config, postData.body);
 
   if (date.type === 'left') return date;
   if (body.type === 'left') return body;
@@ -46,12 +47,14 @@ export async function createPost(
 }
 
 export async function transformContent(
+  config: KestrelConfig,
   content: string,
-  imageRoot: string
 ): Promise<Either<string, string>> {
+  if (!config.images?.imageRoot) {
+    return either.right(content);
+  }
   const rem = remark().use(resolveImageAlias, {
-    resolve: true,
-    imageRoot: imageRoot,
+    imageRoot: config.images.imageRoot,
   });
   const file = await rem.process(content);
   return either.right(String(file));
