11a0d519151c46cac9f331186baafb7a0d73dd63
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-24 17:20:18 -0300

    refactor(action): Await action function; don't return it

--- src/actions.ts
+++ src/actions.ts
@@ -34,7 +34,7 @@ export async function runAction(action: Action): Promise<void> {
     process.exit(1); /* Exit with failure code 1. */
   }
   try {
-    return actionTable[action](config.value);
+    await actionTable[action](config.value);
   }
   catch (error) {
     logger.error(String(error));
