e5d80367cae0bdbb0c91290565bd7b63ee778f6c
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-26 18:47:16 -0300

    feat(cli): Add variadic options for ignore patterns + image extensions

--- src/cli.ts
+++ src/cli.ts
@@ -14,12 +14,13 @@ export function run(): void {
     .argument('[PATH]', 'input folder', '.')
     .option('-d, --destination <PATH>', 'destination folder')
     .option('-p, --posts <PATH>', 'destination folder for posts')
-    .option('-i, --images <PATH>', 'destination folder for images')
+    .option('-m, --images <PATH>', 'destination folder for images')
     .option('-a, --image-alias <PATH>', 'how to resolve image path alias')
     .option('-q, --quiet', 'silence log messages')
+    .option('-i, --ignore <PATTERN...>', 'ignore paths matching pattern(s)')
     .option('--never-infer-date', 'never infer post date from file')
     .option('--optimize-images', 'optimize post images when building')
-    .option('--image-extensions <EXTS>', 'image extensions to look for, comma-separated')
+    .option('--extensions <EXTENSION...>', 'image extensions to look for, comma-separated')
     .action((folder, options) => {
       const config = parseOptions(options);
       return runAction('build', folder, config);
@@ -29,6 +30,7 @@ export function run(): void {
     .description('list all posts with their IDs')
     .alias('ls')
     .argument('[PATH]', 'input folder', '.')
+    .option('-i, --ignore <PATTERN...>', 'ignore paths matching pattern(s)')
     .action((folder, options) => {
       const config = parseOptions(options);
       return runAction('list', folder, config);
@@ -38,6 +40,7 @@ export function run(): void {
     .description('show alpaca status')
     .alias('stat')
     .argument('[PATH]', 'input folder', '.')
+    .option('-i, --ignore <PATTERN...>', 'ignore paths matching pattern(s)')
     .action((folder, options) => {
       const config = parseOptions(options);
       return runAction('status', folder, config);
--- src/config/read-cli.ts
+++ src/config/read-cli.ts
@@ -11,16 +11,9 @@ export type OptionMap = Partial<{
   'never-infer-date': boolean;
   'optimize-images': boolean;
   'image-alias': string;
-  'image-extensions': string;
-  'ignore': string;
+  'image-extensions': string[];
+  'ignore': string[];
 }>;
-
-/* Assumes argument array option like: 'a,b,c,d'.
- * - Allows space(s) after commas.
- * - Forgives trailing comma at the end. */
-function parseArray(arg: string): string[] {
-  return arg.split(/,\s*/).filter(x => x.length > 0);
-}
 
 /* Parse options received from a Commander options object. */
 export function parseOptions(options: OptionMap): Partial<AlpacaConfig> {
@@ -34,9 +27,8 @@ export function parseOptions(options: OptionMap): Partial<AlpacaConfig> {
     neverInferDate: options['never-infer-date'],
     optimizeImages: options['optimize-images'],
     imageAlias: options['image-alias'],
-    imageExtensions: options['image-extensions']
-      ? parseArray(options['image-extensions'])
-      : defaultImageExtensions,
+    imageExtensions: options['image-extensions'],
+    ignore: options['ignore'],
   };
 }
 
