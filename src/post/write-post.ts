bdb6c33aa62339ee9d43d9780a47180b6170e97d
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-31 00:13:53 -0300

    feat(post): Resolve thumbnail src in blog post

--- src/post/write-post.ts
+++ src/post/write-post.ts
@@ -2,10 +2,11 @@ import { Either } from '@/monad/either';
 import * as either from '@/monad/either';
 import { AlpacaConfig } from '@/config/alpaca-config';
 import { getPostDate } from '@/post/post-date';
-import { BlogPost, PostMetadata, PostFile, PostInfo } from '@/post/post-type';
+import { BlogPost, Image, PostMetadata, PostFile, PostInfo } from '@/post/post-type';
 import { ImageMap } from '@/images/write-images';
 import { readPost } from '@/post/read-post';
 import { resolveImageAlias } from '@/images/resolve-alias';
+import { resolveThumbnail } from '@/images/thumbnail';
 import { remark } from 'remark';
 
 export async function compilePost(
@@ -25,21 +26,31 @@ export async function createPost(
   postData: PostFile,
   imageMap: ImageMap,
 ): Promise<Either<string, BlogPost>> {
-  function createMeta(date: Date): PostMetadata {
+  function createMeta(date: Date, thumbnail: Image | undefined): PostMetadata {
     return {
       title: postData.title,
       id: info.id,
       timestamp: date.getTime(),
+      thumbnail: thumbnail,
       description: postData.description ?? '',
       tags: postData.tags ?? [],
     };
   }
-  return either.bindAsync(
+
+  const getMeta = either.bindAsync(
     getPostDate(config, info.path.absolute, postData.date),
-    async (date) => either.bindAsync(
+    async (date) => either.bind(
+      resolveThumbnail(postData.thumbnail, info, imageMap),
+      (thumbnail) => either.right(createMeta(date, thumbnail))
+    )
+  );
+
+  return either.bindAsync(
+    getMeta,
+    async (meta) => either.bindAsync(
       transformContent(info, postData.content, imageMap),
       async (body) => either.right<string, BlogPost>({
-        metadata: createMeta(date),
+        metadata: meta,
         body: body,
       }),
     ),
